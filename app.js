3e120a3ff56b93923c191eee16c8540ac840980b
Phub2014599 <142843189+Phub2014599@users.noreply.github.com>
2024-09-19 08:08:11 +0700

    sua loi

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
-
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
 // handle 404 response
 app.use((req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
@@ -27,8 +29,6 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
-});
+
 
 module.exports = app;
